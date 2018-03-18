5c754e418b6a8352685770439d5e2feb554f19d9
ivo- <ivailo.nikolaev.hristov@gmail.com>
2018-03-18 23:47:37 +0200

    Export redux api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const api = require('./api');
+const redux = require('./redux');
 const connect = require('./react');
 
 
-module.exports = { ...api, connect: api.autocurry(connect) };
+module.exports = { ...api, ...redux, connect: api.autocurry(connect) };
